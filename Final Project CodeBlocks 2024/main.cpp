380f6bf1511d950130844c573d3920b02c2d04fe
Jacob <jakehub24@gmail.com>
2024-06-01 23:38:57 -0400

    Got it working!

--- Final Project CodeBlocks 2024/main.cpp
+++ Final Project CodeBlocks 2024/main.cpp
@@ -1,8 +1,26 @@
-#include <iostream>
+#include <curses.h>
+#include <string>
 
 using namespace std;
 
+
+// winget install ezwinports.make
+
 int main()
 {
+  initscr();
+  cbreak();
+  noecho();
+
+    char in1 = getch();
+    char in2 = getch();
+    string text = "Hello, ncurses!";
+
+    mvprintw(LINES/2,(COLS-text.size())/2,text.c_str());
+
+    move(in1,in2);
+    printf("Shrek");
+    getch();
+    endwin();
     return 0;
 }
