8b55ab1157a2caa817b79306c6c29015c15a70bb
Jacob <jakehub24@gmail.com>
2024-06-02 22:45:27 -0400

    Sunday Night Push

--- Final Project CodeBlocks 2024/main.cpp
+++ Final Project CodeBlocks 2024/main.cpp
@@ -6,21 +6,71 @@ using namespace std;
 
 // winget install ezwinports.make
 
-int main()
+int main(int argc, char** argv)
 {
-  initscr();
-  cbreak();
-  noecho();
+    initscr(); // Start Curses
+    cbreak();
+    noecho();
 
-    char in1 = getch();
-    char in2 = getch();
-    string text = "Hello, ncurses!";
+    int y, x, yBeg, xBeg, yMax, xMax;
 
-    mvprintw(LINES/2,(COLS-text.size())/2,text.c_str());
+    getyx(stdscr, y, x);
+    getbegyx(stdscr, yBeg, xBeg);
+    getmaxyx(stdscr, yMax, xMax);
 
-    move(in1,in2);
-    printf("Shrek");
+    printw("%d %d %d %d %d %d", y, x, yBeg, xBeg, yMax, xMax);
+
+
     getch();
-    endwin();
+
+    getyx(stdscr, y, x);
+    getbegyx(stdscr, yBeg, xBeg);
+    getmaxyx(stdscr, yMax, xMax);
+
+    printw("%d %d %d %d %d %d", y, x, yBeg, xBeg, yMax, xMax);
+
+    getch();
+
+    endwin(); // End curses
+
     return 0;
 }
+
+/*
+This is where all of the functions and their inputs will reside for curses to refer back to
+
+NOTE: curses always has y before x
+
+newwin(height, width, start_y, start_x); // creates a new window
+box(win, left&right, top&bottom); // makes a simple box around a window
+wborder(win, left, right, top, bottom, tlc, trc, blc, brc); // makes a more complex border around  a window
+wprintw(win, text); // prints to a window at 0,0; will overlap borders
+mvwprintw(win, start_y, start_x, text); // prints to a window at x,y
+init_pair(valueOfColourPair, foreground, background); // initializes a colour pair assigned an int value with 2 colours
+getyx(win, y, x); // changes y and x to the current position of the cursor in the window specified
+getbegyx(win, y, x); // changes y and x to the beginning corner of the window specified
+getmaxyx(win, y, x); // changes y and x to the height and width of the window specified
+
+
+
+
+attributes:
+attron();
+attroff();
+A_NORMAL
+A_STANDOUT
+A_REVERSE
+A_BLINK
+A_ALTCHARSET
+
+COLOR_PAIR(valueOfColourPair);
+COLOR_BLACK 0
+COLOR_RED
+COLOR_GREEN
+COLOR_YELLOW
+COLOR_BLUE
+COLOR_MAGENTA
+COLOR_CYAN
+COLOR_WHITE
+
+*/
